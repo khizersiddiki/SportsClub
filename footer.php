e765d2710ec372eb9865b43fc0a4646ccf514b25
khizersiddiki <109090081+khizersiddiki@users.noreply.github.com>
2023-05-19 16:57:10 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,106 @@
+ <!-- Footer Section Begin -->
+ <footer class="footer-section set-bg" data-setbg="img/footer-bg.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="fs-logo">
+                    <div class="logo">
+                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                    <ul>
+                        <li><i class="fa fa-envelope"></i>mkhizer211@gmail.com </li>
+                        <li><i class="fa fa-copy"></i> +92-3152716934</li>
+                        <li><i class="fa fa-thumb-tack"></i>282/9 malir karachi </li>
+                    </ul>
+                    <div class="fs-social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-whatsapp"></i></a>
+                        <a href="#"><i class="fa fa-wechat"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 offset-lg-1">
+                <div class="fs-widget">
+                    <h4>Rayan SportsClub</h4>
+                    <ul class="fw-links">
+                        <li><a href="club.html">About us</a></li>
+                        <li><a href="contact.html">Contact us</a></li>
+                        <li><a href="membership.html">Membership</a></li>
+                     
+                    </ul>
+                    <ul class="fw-links">
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="fs-widget">
+                    <h4></h4>
+
+                    <div class="fw-item">
+                        <h5><a href="#"></a></h5>
+                        <ul>
+                            <li><i class="fa fa-calendar">20-june-2022</i> </li>
+                            <li><i class="fa fa-edit" style="color:grey;" >SILVER JUBLIEE CERMONY</i> </li>
+                        </ul>
+                    </div>
+                    <div class="fw-item">
+                        <h5><a href="#"></a></h5>
+                        <ul>
+                            <li><i class="fa fa-calendar">11-june-2022</i></li>
+                            <li><i class="fa fa-edit"></i>Best Sports CLub in city </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="copyright-option">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="co-widget">
+                        <ul>
+                            <li><a href="contact.html">Karachi Office</a></li>
+                            <li><a href="contact.html">Lahore Office`</a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
+
+
+    <!-- Search model Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch"><i class="fa fa-close"></i></div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search model end -->
+        <script>// MDB Lightbox Init
+          $(function () {
+           $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+          });</script>
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,128 @@
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    $(".canvas-open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+
+    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+    // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+
+   
+    $(".news-slider").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false
+    });
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+   
+    $(".video-slider").owlCarousel({
+        items: 4,
+        dots: false,
+        autoplay: false,
+        margin: 0,
+        loop: true,
+        smartSpeed: 1200,
+        nav: true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            480: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 4,
+            },
+        }
+    });
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+})(jQuery);
